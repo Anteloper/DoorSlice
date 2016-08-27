f8bddfd2a98b97461e64799d7c0800c5bd4f331c
Oliver Hill <obh6@georgetown.edu>
2016-08-27 12:54:12 -0400

    Changed app delegate so WelcomeController is the first screen the first time they app is launched

--- Slice/AppDelegate.swift
+++ Slice/AppDelegate.swift
@@ -17,44 +17,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        //Initialize Singleton Classes
         Stripe.setDefaultPublishableKey(Constants.stripePublishableKey)
         _ = CurrentPrices()
         
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+       
+        if checkFirstLaunch(){ return true }
+        
         guard let user = NSKeyedUnarchiver.unarchiveObjectWithFile(Constants.userFilePath()) as? User else{
-            noUserFound()
+            window?.rootViewController = LoginController()
+            window!.makeKeyAndVisible()
+            checkiPhone4()
             return true
         }
         
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
         if user.isLoggedIn{
-            if user.hasSeenTutorial{
-                containerController = ContainerController()
-                containerController!.loggedInUser = user
-                window?.rootViewController = containerController!
-            }
-            else{
-                let tc = TutorialController()
-                tc.user = user
-                window?.rootViewController = tc
-            }
+            setupWithUser(user)
         }
+        
         else{
             window?.rootViewController = LoginController()
         }
+        
         window!.makeKeyAndVisible()
-        
-        if UIScreen.mainScreen().bounds.height <= 480.0{ Alerts.iPhone4() }
-        
+        checkiPhone4()
         return true
     }
     
     
-    func noUserFound(){
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window?.rootViewController = LoginController()
-        window!.makeKeyAndVisible()
+    func checkFirstLaunch()->Bool{
+        let isNotFirstLaunch = NSUserDefaults.standardUserDefaults().boolForKey("isNotFirstLaunch")
+        if !isNotFirstLaunch{
+            NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isNotFirstLaunch")
+            window?.rootViewController = WelcomeController()
+            window!.makeKeyAndVisible()
+            checkiPhone4()
+        }
+        return !isNotFirstLaunch
     }
     
+    func setupWithUser(user: User){
+        if user.hasSeenTutorial{
+            containerController = ContainerController()
+            containerController!.loggedInUser = user
+            window?.rootViewController = containerController!
+        }
+        else{
+            let tc = TutorialController()
+            tc.user = user
+            window?.rootViewController = tc
+        }
+    }
+    
+    func checkiPhone4(){ if UIScreen.mainScreen().bounds.height <= 480.0{ Alerts.iPhone4() } }
     
     func application(application: UIApplication, willChangeStatusBarFrame newStatusBarFrame: CGRect) {
         let windows = UIApplication.sharedApplication().windows
