a6e7deed7be3053d10c1cba7bb52bb81711e9e23
Oliver Hill <obh6@georgetown.edu>
2016-10-19 11:24:35 -0400

    Removed xcuserstate

--- Slice/AppDelegate.swift
+++ Slice/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var containerController: ContainerController?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
+    
         Stripe.setDefaultPublishableKey(Constants.stripePublishableKey)
         retrieveAndSetPrices()
         
